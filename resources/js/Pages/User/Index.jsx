468707b242e9699c943bbaa72770ae79768c1782
siwakasen <siwakasen@gmail.com>
2024-08-25 16:13:17 +0800

    add total users and create button

--- resources/js/Pages/User/Index.jsx
+++ resources/js/Pages/User/Index.jsx
@@ -2,6 +2,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
 import TableUsers from '@/Components/Users/TableUsers';
 import Pagination from '@/Components/Pagination';
+import { Link } from '@inertiajs/react';
 export default function UserIndex({ auth, users }) {
     return (
         <AuthenticatedLayout
@@ -13,7 +14,17 @@ export default function UserIndex({ auth, users }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    {/* make styling on label */}
+                    <div className='p-2 flex'>
+                        <label className='text-lg font-semibold'>Total Users : {users.total}</label>
+                        {/* button create */}
+                        <Link href={route('users.create')} className='ml-auto bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded'>
+                            Create User
+                        </Link>
+                    </div>
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="">
+                        </div>
                         <TableUsers users={users} />
                         <Pagination links={users.links} />
                     </div>
